2c120bee89b82720782a2166f0bb4ccf69c75b2b
Angelo Mateo Toenbreker <mateotoenbreker@gmail.com>
2026-05-19 06:43:05 -0700

    Add build, platform outputs and installers (tracked by LFS where applicable)

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter_windows_3.38.5-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+3" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 3 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter_windows_3.38.5-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME"
+  "FLUTTER_ROOT=C:\\flutter_windows_3.38.5-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME"
+  "FLUTTER_TARGET=C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9WRVJTSU9OPTMuMzguNQ==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZjZmZjE1MjlmZA==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049MTUyN2FlMGVjNQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC40"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Angelo Toenbreker\\StudioProjects\\LIME\\.dart_tool\\package_config.json"
+)
